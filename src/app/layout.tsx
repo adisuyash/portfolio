0dc08abb94c01db1b4afefa4774a4d3755f10709
adisuyash <adityasuyashgupta@gmail.com>
2024-10-15 02:19:50 +0530

    Add @vercel/analytics package for web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -64,6 +65,8 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system">
           <TooltipProvider delayDuration={0}>
             {children}
+            {/* --- For Vercel Web Analytics --- */}
+            <Analytics /> 
             <Navbar />
           </TooltipProvider>
         </ThemeProvider>
