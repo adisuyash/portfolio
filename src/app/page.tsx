1247c7e19eb296b5c65925c96895243bea9d31d0
adisuyash <adityasuyashgupta@gmail.com>
2024-10-15 14:04:03 +0530

    removed unwanted sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -179,7 +179,7 @@ export default function Page() {
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   I&apos;ve worked on a variety of projects, from simple
                   websites to complex web applications. Here are a few of my
-                  favorites.
+                  projects.
                 </p>
               </div>
             </div>
@@ -206,7 +206,7 @@ export default function Page() {
           </div>
         </div>
       </section>
-      <section id="hackathons">
+      {/* <section id="hackathons">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 13}>
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
@@ -248,7 +248,7 @@ export default function Page() {
             </ul>
           </BlurFade>
         </div>
-      </section>
+      </section> */}
       <section id="contact">
         <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 16}>
